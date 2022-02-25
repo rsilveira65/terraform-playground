2168b4afb5b3f87c695ed3a63bf714b9051f37c7
rsilveira65 <rsilveiracc@gmail.com>
2022-02-25 12:58:03 -0300

    format files.

--- infrastructure/modules/eks/nodes/node_group.tf
+++ infrastructure/modules/eks/nodes/node_group.tf
@@ -1,24 +1,24 @@
 resource "aws_eks_node_group" "cluster" {
-  
-    cluster_name = var.eks_cluster.name
-    node_group_name = format("%s-node-group", var.cluster_name)
-    node_role_arn = aws_iam_role.eks_nodes_roles.arn
 
-    subnet_ids = [
-        var.private_subnet_1a.id,
-        var.private_subnet_1c.id
-    ]
+  cluster_name    = var.eks_cluster.name
+  node_group_name = format("%s-node-group", var.cluster_name)
+  node_role_arn   = aws_iam_role.eks_nodes_roles.arn
 
-    instance_types = var.nodes_instances_sizes
+  subnet_ids = [
+    var.private_subnet_1a.id,
+    var.private_subnet_1c.id
+  ]
 
-    scaling_config {
-        desired_size    = lookup(var.auto_scale_options, "desired")
-        max_size        = lookup(var.auto_scale_options, "max")
-        min_size        = lookup(var.auto_scale_options, "min")
-    }
+  instance_types = var.nodes_instances_sizes
 
-    tags = {
-        "kubernetes.io/cluster/${var.cluster_name}" = "owned"
-    }
+  scaling_config {
+    desired_size = lookup(var.auto_scale_options, "desired")
+    max_size     = lookup(var.auto_scale_options, "max")
+    min_size     = lookup(var.auto_scale_options, "min")
+  }
+
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+  }
 
 }
--- infrastructure/modules/network/private.tf
+++ infrastructure/modules/network/private.tf
@@ -1,31 +1,31 @@
 resource "aws_subnet" "private_subnet_1a" {
-    vpc_id = aws_vpc.cluster_vpc.id
-    cidr_block = lookup(var.private_subnets_cidr_blocks, "private_subnet_1a")
+  vpc_id     = aws_vpc.cluster_vpc.id
+  cidr_block = lookup(var.private_subnets_cidr_blocks, "private_subnet_1a")
 
-    availability_zone = format("%sa", var.aws_region)
+  availability_zone = format("%sa", var.aws_region)
 
-    tags = {
-        Name = format("%s-private-1a", var.cluster_name)
-    }
+  tags = {
+    Name = format("%s-private-1a", var.cluster_name)
+  }
 }
 
 resource "aws_subnet" "private_subnet_1c" {
-    vpc_id = aws_vpc.cluster_vpc.id
-    cidr_block = lookup(var.private_subnets_cidr_blocks, "private_subnet_1c")
+  vpc_id     = aws_vpc.cluster_vpc.id
+  cidr_block = lookup(var.private_subnets_cidr_blocks, "private_subnet_1c")
 
-    availability_zone = format("%sc", var.aws_region)
+  availability_zone = format("%sc", var.aws_region)
 
-    tags = {
-        Name = format("%s-private-1c", var.cluster_name)
-    }
+  tags = {
+    Name = format("%s-private-1c", var.cluster_name)
+  }
 }
 
 resource "aws_route_table_association" "private1a" {
-  subnet_id = aws_subnet.private_subnet_1a.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
   route_table_id = aws_route_table.nat.id
 }
 
 resource "aws_route_table_association" "private1c" {
-  subnet_id = aws_subnet.private_subnet_1c.id
+  subnet_id      = aws_subnet.private_subnet_1c.id
   route_table_id = aws_route_table.nat.id
 }
